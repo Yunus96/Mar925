0871840b1453696d8bf5fafb7059bebea3f8b0c9
Yunus96 <mohammadyunus102@gmail.com>
2025-04-01 22:06:07 +0530

    restructuring

--- controller/github.controller.js
+++ controller/github.controller.js
@@ -14,6 +14,7 @@ const getGitHubProfile =  async (req, res) => {
 
         if (cachedData == null) {
             // Fetch Data from GitHub API
+            console.log("hi")
             const [followersRes, followingRes, reposRes] = await Promise.all([
                 axios.get(`${GITHUB_API}/followers`),
                 axios.get(`${GITHUB_API}/following`),
@@ -44,4 +45,74 @@ const getGitHubProfile =  async (req, res) => {
     }
 };
 
-export {getGitHubProfile }
+const getRepoDetails = async (req, res) => {
+    try {
+        const { repoName } = req.params;
+        
+        const cacheKey = `github-repo:${process.env.GITHUB_USERNAME}:${repoName}`;
+    
+        // Check Redis Cache
+        const cachedData = await redisClient.get(cacheKey);
+        
+        if (cachedData == null) {
+            // Fetch repo details from GitHub API
+            const repoRes = await axios.get(`https://api.github.com/repos/${process.env.GITHUB_USERNAME}/${repoName}`);
+            //console.log(repoRes)
+
+            const repoData = {
+                name: repoRes.data.name,
+                description: repoRes.data.description,
+                url: repoRes.data.html_url,
+                stars: repoRes.data.stargazers_count,
+                forks: repoRes.data.forks_count,
+                language: repoRes.data.language,
+                created_at: repoRes.data.created_at,
+                updated_at: repoRes.data.updated_at,
+                cached: "false"
+            };
+
+            // Store Data in Redis (Expire in 10 Minutes)
+            await redisClient.setEx(cacheKey, CACHE_TTL, JSON.stringify(repoData));
+
+            res.json(repoData);
+        } else {
+             return res.json(JSON.parse(cachedData)); // Return Cached Data
+        }
+    } catch (error) {
+        console.error("GitHub API Error:", error.message);
+        res.status(500).json({ message: "Error fetching repository details" });
+    }
+}
+
+const postIssue = async (req, res) =>{
+    try {
+        const { repoName } = req.params;
+        const { title, body } = req.body;
+        
+        if (!title || !body) {
+            return res.status(400).json({ message: "Title and body are required" });
+        }
+
+        const response = await axios.post(
+            `https://api.github.com/repos/${process.env.GITHUB_USERNAME}/${repoName}/issues`,
+            { title, body },
+            {
+                headers: {
+                    Authorization: `token ${process.env.GITHUB_TOKEN}`,
+                    Accept: "application/json"
+                }
+            }
+        );
+
+        res.json({
+            message: "Issue created successfully",
+            issue_url: response.data.html_url
+        });
+    } catch (error) {
+        console.error("GitHub API Error:", error.response?.data || error.message);
+        res.status(500).json({ message: "Error creating GitHub issue", error: error.response?.data });
+    }
+}
+
+
+export {getGitHubProfile, getRepoDetails, postIssue }
--- routes/github.routes.js
+++ routes/github.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { getGitHubProfile } from '../controller/github.controller.js';
+import { getGitHubProfile, getRepoDetails, postIssue } from '../controller/github.controller.js';
 //getRepoDetails, postIssue
 const router = express.Router();
 
 router.get("/github", getGitHubProfile);
-//router.get("/github/:repoName", getRepoDetails);
-//router.post("/github/:repoName/issues", postIssue);
+router.get("/github/:repoName", getRepoDetails);
+router.post("/github/:repoName/issues", postIssue);
 
 export default router;
